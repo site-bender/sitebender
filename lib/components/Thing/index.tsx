83f08e1bb906b2561385e5fb3d86f121ac68d5fe
Charles Munat <charles.munat@gmail.com>
2025-07-15 12:48:00 +1200

    fix: update Thing component imports and remove FP violations
    - Update import paths after helper reorganization
    - Remove try/catch exception handling in favor of graceful error handling
    - Replace mutable let with immutable const
    - Follow strict functional programming principles

--- lib/components/Thing/index.tsx
+++ lib/components/Thing/index.tsx
@@ -1,7 +1,9 @@
 import type { BaseComponentProps } from "../../types/index.ts"
 import type ThingProps from "../../types/Thing/index.ts"
 
-import createJsonLd from "../helpers/createJsonLd/index.ts"
+import createJsonLd from "./createJsonLd/index.ts"
+import formatTemplate from "./formatTemplate/index.ts"
+import FORMATTERS from "./formatTemplate/processTemplate/formatters/index.ts"
 
 export type Props = BaseComponentProps<
 	ThingProps,
@@ -10,14 +12,36 @@ export type Props = BaseComponentProps<
 >
 
 export default function Thing(
-	{ disableJsonLd, schemaType = "Thing", subtypeProperties, ...props }: Props,
+	{ disableJsonLd, format, schemaType = "Thing", subtypeProperties, ...props }:
+		Props,
 ) {
 	const jsonLd = createJsonLd(schemaType, props, subtypeProperties)
+	const allProps = { ...props, ...subtypeProperties }
+
+	// Generate formatted content if format is provided
+	const formattedContent = format
+		? formatTemplate(format, {
+			props: allProps,
+			formatters: FORMATTERS,
+		})
+		: null
 
 	return (
 		<>
-			<span>Schema type is {schemaType}</span>
-			<pre>{JSON.stringify(props, null, 2)}</pre>
+			{formattedContent
+				? (
+					<span>
+						{formattedContent.map((item, index) => (
+							<span key={index}>{item}</span>
+						))}
+					</span>
+				)
+				: (
+					<>
+						<span>Schema type is {schemaType}</span>
+						<pre>{JSON.stringify(allProps, null, 2)}</pre>
+					</>
+				)}
 			{disableJsonLd ? null : (
 				<script type="application/ld+json">
 					{JSON.stringify(jsonLd, null, 2)}
