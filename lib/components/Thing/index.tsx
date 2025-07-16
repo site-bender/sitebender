25bcd5eef854944b0d41e36291ad2c453a19aab6
Charles Munat <charles.munat@gmail.com>
2025-07-16 13:27:12 +1200

    refactor: migrate Thing component to use Base
    - Remove duplicate formatting logic
    - Maintain existing API and functionality
    - Thing now extends Base with JSON-LD generation

--- lib/components/Thing/index.tsx
+++ lib/components/Thing/index.tsx
@@ -1,9 +1,8 @@
 import type { BaseComponentProps } from "../../types/index.ts"
 import type ThingProps from "../../types/Thing/index.ts"
 
+import Base from "../Base/index.tsx"
 import createJsonLd from "./createJsonLd/index.ts"
-import formatTemplate from "./formatTemplate/index.ts"
-import FORMATTERS from "./formatTemplate/processTemplate/formatters/index.ts"
 
 export type Props = BaseComponentProps<
 	ThingProps,
@@ -18,30 +17,9 @@ export default function Thing(
 	const jsonLd = createJsonLd(schemaType, props, subtypeProperties)
 	const allProps = { ...props, ...subtypeProperties }
 
-	// Generate formatted content if format is provided
-	const formattedContent = format
-		? formatTemplate(format, {
-			props: allProps,
-			formatters: FORMATTERS,
-		})
-		: null
-
 	return (
 		<>
-			{formattedContent
-				? (
-					<span>
-						{formattedContent.map((item, index) => (
-							<span key={index}>{item}</span>
-						))}
-					</span>
-				)
-				: (
-					<>
-						<span>Schema type is {schemaType}</span>
-						<pre>{JSON.stringify(allProps, null, 2)}</pre>
-					</>
-				)}
+			<Base format={format} props={allProps} />
 			{disableJsonLd ? null : (
 				<script type="application/ld+json">
 					{JSON.stringify(jsonLd, null, 2)}
