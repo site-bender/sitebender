4a462cd74ba2659df0ea3f6d01f21f85bda3aef3
Charles Munat <charles.munat@gmail.com>
2025-09-08 16:41:21 +1200

    refactor(toolkit): extract parsePair to its own file

--- libraries/toolkit/src/random/generateShortId/generateBase58Uuid/hexToBytes/index.ts
+++ libraries/toolkit/src/random/generateShortId/generateBase58Uuid/hexToBytes/index.ts
@@ -1,14 +1,12 @@
 import map from "../../../../simple/array/map/index.ts"
 import extractPairs from "./extractPairs/index.ts"
+import parsePair from "./parsePair/index.ts"
 
 //++ Converts a hex string to a Uint8Array of bytes
 export default function hexToBytes(hexString: string): Uint8Array {
-	function parsePair(pair: string): number {
-		return parseInt(pair, 16)
-	}
-
 	const pairs = extractPairs(hexString)
 	const bytes = map(parsePair)(pairs)
+	
 	return new Uint8Array(bytes)
 }
 
