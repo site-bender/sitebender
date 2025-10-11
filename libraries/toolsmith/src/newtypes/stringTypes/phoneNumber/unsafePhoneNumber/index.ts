139294af834cb65013e3c47354983175b3fe9afb
Charles Munat <charles.munat@gmail.com>
2025-10-11 21:28:44 +1300

    to be quashed

---
+++ libraries/toolsmith/src/newtypes/stringTypes/phoneNumber/unsafePhoneNumber/index.ts
@@ -0,0 +1,8 @@
+import type { PhoneNumber } from "@sitebender/toolsmith/types/branded/index.ts"
+
+//++ Unsafe constructor that brands a string as PhoneNumber without validation - use only when input is guaranteed valid
+export default function unsafePhoneNumber(): (value: string) => PhoneNumber {
+	return function unsafeToPhoneNumber(value: string): PhoneNumber {
+		return value as PhoneNumber
+	}
+}
