0aa265e7975965789517b9ea6ebba23d2f76deee
Charles Munat <charles.munat@gmail.com>
2025-11-07 13:23:55 +1300

    to be quashed

--- libraries/architect/src/_html/scripting/_Slot/index.ts
+++ libraries/architect/src/_html/scripting/_Slot/index.ts
@@ -1,5 +1,7 @@
+import isDefined from "@sitebender/toolsmith/predicates/isDefined/index.ts"
 import type { VirtualNode } from "@sitebender/toolsmith/types/virtualNode/index.ts"
 import type { BaseProps } from "@sitebender/architect/_html/types/index.ts"
+import _validateAriaAttributes from "../../_validateAriaAttributes/index.ts"
 import _validateAttributes from "../../_validateAttributes/index.ts"
 import _validateRole from "../../_validateRole/index.ts"
 
@@ -13,11 +15,17 @@ export type Props =
  + HTML slot element wrapper for shadow DOM slots
  */
 export default function _Slot(props: Props): VirtualNode {
-	const { children = [], role, ...attrs } = props
+	const { children = [], role, aria, ...attrs } = props
 	const roleAttrs = _validateRole("slot")(role)
+
+	const ariaAttrs = isDefined(aria)
+		? _validateAriaAttributes("slot")(role)(aria)
+		: {}
+
 	const attributes = {
 		..._validateAttributes("slot")(attrs),
 		...roleAttrs,
+		...ariaAttrs,
 	}
 
 	return {
